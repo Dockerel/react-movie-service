5b101bbce8cc0e6083e92df326a17843ffc81a0d
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-10-16 13:10:01 +0900

    react prac finished

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [dollar, setDollars] = useState(1);
-  const [price, setPrice] = useState(1);
-
-  const onCoinChange = (event) => {
-    console.log(event.target.value);
-    const coinInfoArray = event.target.value.split(" ");
-    setPrice(Number(coinInfoArray[coinInfoArray.length - 2]));
-  };
-
-  const onDollarChange = (event) => {
-    setDollars(event.target.value);
-  };
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-
   return (
-    <div>
-      <h1>The Coins! {loading ? null : `(${coins.length})`} </h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select onChange={onCoinChange}>
-          <option>Choose your Coin...</option>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-      <br />
-      <input type="number" value={dollar} onChange={onDollarChange}></input>
-      <h2>{dollar / price}</h2>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/routes/Detail.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState("");
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year?movie_id=${id}`
+      )
+    ).json();
+    const movies = json.data.movies;
+    const targetMovie = movies.find((movie) => movie.id === Number(id));
+    setMovie(targetMovie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      <a href="/">
+        <span>Home</span>
+      </a>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <h2>{movie.title}</h2>
+          <p>{movie.summary}</p>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+export default Detail;
